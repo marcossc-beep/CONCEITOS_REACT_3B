42fb82dec97f3a4e966da858e5bdd2e2b5b1fcaa
unknown <Professor@SN-392513.edu.fiesc.com.br>
2026-05-05 12:27:05 -0300

    pa

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Login from './paginas/Login'
 import Cadastro from './paginas/Cadastro'
+import Usuarios from './paginas/Usuarios'
 
 function App() {
   const [tela, setTela] = useState('login')
@@ -11,6 +12,8 @@ function App() {
       return <Login/>
     } else if (tela === 'cadastro') {
       return <Cadastro/>
+    } else if (tela === 'usuarios') {
+      return <Usuarios/>
     }
   }
   const trocarTela = (pagina) => {
@@ -22,6 +25,10 @@ function App() {
       <h1>pikachu</h1>
       <button onClick={ () => trocarTela('login') }>login</button>
       <button onClick={ () => trocarTela('cadastro') }>cadastro</button>
+      <button onClick={ () => trocarTela('usuarios') }>Usuarios</button>
+      
+      <hr />
+      
       {renderizarTela()}
     </>
   )
